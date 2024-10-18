c8fb384b14960c04cd403ff62dba451fb28a871b
Arjun Bharadwaj <arjunkrish2003@gmail.com>
2024-10-18 14:54:19 +0530

    format code.

--- src/api.js
+++ src/api.js
@@ -1,16 +1,14 @@
-
 async function getWeatherData(location) {
   try {
-        const response = await fetch(
-        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=6WJAD4LXLFVS4KJTA55GZD6XV&contentType=json`
-      );
-
-      if(!response.ok) throw new Error("Weather data not found");
-      return await response.json();
-    }catch (error) {
-        console.error("An error occurred:", error);
-        throw error;
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=6WJAD4LXLFVS4KJTA55GZD6XV&contentType=json`
+    );
+    if (!response.ok) throw new Error("Weather data not found");
+    return await response.json();
+  } catch (error) {
+    console.error("An error occurred:", error);
+    throw error;
   }
 }
 
-export {getWeatherData};
+export { getWeatherData };
