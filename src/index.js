78f0ff6ffe06887f33b81fa670d45a5eadf94e15
Arjun Bharadwaj <arjunkrish2003@gmail.com>
2024-10-19 23:05:57 +0530

    Format code.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
 import "./styles.css";
-import {getWeatherData} from "./api";
+import { getWeatherData } from "./api";
 import { processWeatherData, displayWeatherDetails } from "./weatherData";
 
 const weatherForm = document.querySelector("#weather-form");
 const locationInput = document.querySelector("#location-input");
 
-weatherForm.addEventListener('submit', async(e)=>{
-    e.preventDefault();
-    const location = locationInput.value.trim();
-    if(!location)
-        return;
+weatherForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const location = locationInput.value.trim();
+  if (!location) return;
 
-    try{
-        const weatherData = await getWeatherData(location);
-        const processData = processWeatherData(weatherData);
-        displayWeatherDetails(processData);
-    }
-    catch(error){
-        alert('Error fetching the data. Please try again!')
-    }
-})
+  try {
+    const weatherData = await getWeatherData(location);
+    const processData = processWeatherData(weatherData);
+    displayWeatherDetails(processData);
+  } catch (error) {
+    alert("Error fetching the data. Please try again!");
+  }
+});
